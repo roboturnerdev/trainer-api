78a4b562fc747590a0c301fc9332bf200a93e3cf
Robert Turner <roboturnerdev@gmail.com>
2021-11-11 19:28:05 -0500

    db works and sends the object back. might need to stringify

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 const express = require('express');
-const loki = require('loki');
+const Loki = require('lokijs');
 
 const app = express();
+const db = new Loki('cg');
+const trainersDb = db.addCollection('trainers');
 
+trainersDb.insert({
+    email: 'one@cg.com',
+    phone: '1234567890',
+    first: 'john',
+    last: 'doe'
+});
 
+db.serialize();
+
+const foundTrainer = trainersDb.findOne({});
+console.log(foundTrainer);
+
+// {
+//     "id" : "trainer-id-000001",
+//     "email" : "trainer@campgladiator.com",
+//     "phone" : "5125125120",
+//     "first_name": "Fearless",
+//     "last_name": "Contender"
+// }
