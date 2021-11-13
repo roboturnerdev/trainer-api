a005c5fa9dbb9bf84e25d0693f39f5dbee8353de
Robert Turner <roboturnerdev@gmail.com>
2021-11-13 01:16:56 -0500

    3 seeds working for index

--- repositories/TrainersRepo.js
+++ repositories/TrainersRepo.js
@@ -8,9 +8,31 @@ class TrainersRepo {
         const context = new Loki('cg', {autosave: true, autoload: true, throttledSaves: true});
         this.trainersDb = context.addCollection('trainers');
         console.log('loki made the db and connected');
+
+        //test seeds
+        const newTrainer1 = this.trainersDb.insert({
+            email: 'trainer@campgladiator.com',
+            phone: '1234567890',
+            firstName: 'Fearless',
+            lastName: 'Contender',
+            address: '123 Anywhere St, USA'
+        });
+        const newTrainer2 = this.trainersDb.insert({
+            email: 'roboturnerdev@gmail.com',
+            phone: '9198668019',
+            firstName: 'Robert',
+            lastName: 'Turner',
+            address: '500 Main St, Virginia'
+        });
+        const newTrainer3 = this.trainersDb.insert({
+            email: 'ap@test.com',
+            phone: '1231231234',
+            firstName: 'Anne Porter',
+            lastName: 'Turner',
+            address: '3131 Mountain Top Pl, North Carolina'
+        });
     }
     async getTrainers() {
-        console.log('inside Repository.getTrainers()');
         const foundTrainers = await this.trainersDb.find({});
         console.log('repository trainers: ', foundTrainers);
         return foundTrainers;
--- routes/router/index.js
+++ routes/router/index.js
@@ -3,12 +3,12 @@ const router = express.Router();
 const TrainersController = require('../../controllers/Trainers');
 const catchAsync = require('../../public/scripts/catchAsync');
 
-// router.route('/')
-    // .get(catchAsync(TrainersController.getTrainers()));
+router.route('/')
+    .get(catchAsync(TrainersController.getTrainers));
 
 router.all('/', (req, res) => {
     console.log('router all');
-    res.send('router sent response')
+    res.send('router sent response');
 });
 
 module.exports = router;
