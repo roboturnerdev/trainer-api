5b263e96b735d79def1d49716954a843fe670164
Robert Turner <roboturnerdev@gmail.com>
2021-11-13 00:52:06 -0500

    router is connected again i fixed a bit of the structure

---
+++ routes/router/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const TrainersController = require('../../controllers/Trainers');
+const catchAsync = require('../../public/scripts/catchAsync');
+
+// router.route('/')
+    // .get(catchAsync(TrainersController.getTrainers()));
+
+router.all('/', (req, res) => {
+    console.log('router all');
+    res.send('router sent response')
+});
+
+module.exports = router;
