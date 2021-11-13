381bf6f8bd2520615b9ed5d7c32a2f36f8db74b7
Robert Turner <roboturnerdev@gmail.com>
2021-11-12 20:40:12 -0500

    db up, express init, trainer controller begun

---
+++ routes/routes-template/index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+
+let router = express.Router();
+
+// routes
+
+module.exports = router;
---
+++ services/TrainersService.js
@@ -0,0 +1,6 @@
+// need interfaces, betty being one of them
+const TrainersRepo = require('../api/interfaces/repos/ITrainersRepo');
+// need model
+const Trainer = require('../api/models/trainer');
+
+class 
